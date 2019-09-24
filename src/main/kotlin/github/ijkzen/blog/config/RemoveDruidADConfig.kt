3c89f35655d082501d32f68317d66e77b63901cf
xiongshanghua <xiongshanghua@sz.hitrontech.com>
2019-09-24 16:43:13 +0800

    feat(druid): remove advertisement of druid monitor footer

--- src/main/kotlin/github/ijkzen/blog/config/RemoveDruidADConfig.kt
+++ src/main/kotlin/github/ijkzen/blog/config/RemoveDruidADConfig.kt
@@ -1,14 +1,17 @@
 package github.ijkzen.blog.config
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure
 import com.alibaba.druid.spring.boot.autoconfigure.properties.DruidStatProperties
+import org.springframework.boot.autoconfigure.AutoConfigureAfter
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication
 import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 
-//@Configuration
-//@AutoConfigureAfter(DruidDataSourceAutoConfigure::class)
-class RemoveDruidADConfig {
+@Configuration
+@AutoConfigureAfter(DruidDataSourceAutoConfigure::class)
+private class RemoveDruidADConfig {
 
     @Bean
     @ConditionalOnWebApplication
@@ -19,7 +22,7 @@ class RemoveDruidADConfig {
     fun removeDruidAD(druidStatProperties: DruidStatProperties): FilterRegistrationBean<DruidADFilter> {
         val config = druidStatProperties.statViewServlet
         val pattern = if (config.urlPattern != null) config.urlPattern else "/druid/*"
-        val commonJsPattern = pattern.replace("\\*", "js/common.js")
+        val commonJsPattern = pattern.replace("*", "js/common.js")
         val registrationBean = FilterRegistrationBean<DruidADFilter>()
         registrationBean.filter = DruidADFilter()
         registrationBean.addUrlPatterns(commonJsPattern)
