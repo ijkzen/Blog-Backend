95ff1cd6ec2791fe8c714df69159008ff53f4c52
xiongshanghua <xiongshanghua@sz.hitrontech.com>
2019-09-25 17:57:37 +0800

    feat(bean): new article bean

---
+++ src/main/kotlin/github/ijkzen/blog/bean/articles/Article.kt
@@ -0,0 +1,57 @@
+package github.ijkzen.blog.bean.articles
+
+import java.util.*
+import javax.persistence.*
+
+/**
+ * @Author ijkzen
+ * @Date 2019/9/25
+ */
+@Entity
+data class Article(
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        var fileName: String?,
+
+        @Column(unique = true)
+        var id: Long?,
+
+        @Column
+        var title: String?,
+
+        @Column
+        var category: String?,
+
+        @Column
+        var visits: Long?,
+
+        @Column
+        var commentId: Long?,
+
+        @Column(columnDefinition = "timestamp default CURRENT_TIMESTAMP")
+        @Temporal(TemporalType.TIMESTAMP)
+        var createdTime: Date?,
+
+        @Column(columnDefinition = "timestamp default CURRENT_TIMESTAMP")
+        @Temporal(TemporalType.TIMESTAMP)
+        var updatedTime: Date?,
+
+        @Column(columnDefinition = "text not null")
+        var content: String?,
+
+        @Column(columnDefinition = "text not null")
+        var `abstract`: String?
+) {
+    constructor() : this(
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null
+    )
+}
