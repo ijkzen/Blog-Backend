1e4af942645234e72dc7c16f1cd7e2e55c062a7d
ijkzen <krystalandhola@outlook.com>
2019-10-07 22:07:29 +0800

    tmp commit

--- src/main/kotlin/github/ijkzen/blog/utils/SecurityUtils.kt
+++ src/main/kotlin/github/ijkzen/blog/utils/SecurityUtils.kt
@@ -1,32 +1,24 @@
 package github.ijkzen.blog.utils
 
-import java.security.SecureRandom
 import javax.crypto.Cipher
 import javax.crypto.SecretKeyFactory
 import javax.crypto.spec.DESKeySpec
 
 class SecurityUtils {
 
-    init {
-        System.err.println("input length: ${key.length}")
-    }
-
     companion object {
-        var key = "Talk is cheap, show me the code"
-
-        private val random = SecureRandom()
+        private const val key = "Talk is cheap, show me the code"
 
         fun encryption(plainText: String): String {
             val cipher = Cipher.getInstance(DES)
-            cipher.init(Cipher.ENCRYPT_MODE, getSecretKey(), random)
-
+            cipher.init(Cipher.ENCRYPT_MODE, getSecretKey())
             return String(cipher.doFinal(plainText.toByteArray()))
         }
 
         fun decryption(cipherText: String): String {
             val cipher = Cipher.getInstance(DES)
-            cipher.init(Cipher.DECRYPT_MODE, getSecretKey(), random)
-
+            cipher.init(Cipher.DECRYPT_MODE, getSecretKey())
+            System.err.println("input length: ${cipherText.toByteArray().size}")
             return String(cipher.doFinal(cipherText.toByteArray()))
         }
 
