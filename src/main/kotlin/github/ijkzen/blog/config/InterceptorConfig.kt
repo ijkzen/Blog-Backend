29619298ec3f61b499da8a6d52af1c38fb9e8449
xiongshanghua <xiongshanghua@sz.hitrontech.com>
2020-01-11 19:39:04 +0800

    fix(resource): remove EnableMvc annotation for InterceptorConfig to load static resource

--- src/main/kotlin/github/ijkzen/blog/config/InterceptorConfig.kt
+++ src/main/kotlin/github/ijkzen/blog/config/InterceptorConfig.kt
@@ -7,7 +7,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
-@EnableWebMvc
 @Configuration
 class InterceptorConfig : WebMvcConfigurer {
 
